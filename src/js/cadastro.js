af0baeb4d952fc655fbe1234fed55e19baea0433
Jahn16 <jpdeoliveiraferreira@gmail.com>
2025-06-13 09:00:29 -0300

    feat: validade player count

--- src/js/cadastro.js
+++ src/js/cadastro.js
@@ -1,74 +1,25 @@
-// Trabalho Interdisciplinar 1 - Aplicações Web
-// CRUD para Jogos de Tabuleiro usando JSONServer
-// Adapte a URL para o endpoint /jogos na sua API JSONServer!
-const apiUrl = 'https://206f06c1-1d9d-4805-869f-1aee322c1c18-00-3oteliflx6o43.riker.replit.dev/jogos';
-// Exemplo: const apiUrl = 'https://jsonserver.rommelpuc.repl.co/jogos';
+const validarQuantidade = (quantidades) => {
+  if (quantidades.length > 2) {
+    return false;
+  }
+  for (const quantidade of quantidades) {
+    if (isNaN(quantidade)) {
+      return false;
+    }
+  }
+  if (quantidades.length == 2) {
+    return Number(quantidades[1]) > Number(quantidades[0]);
+  }
+  return true;
+};
 
-function displayMessage(mensagem) {
-    const msg = document.getElementById('msg');
-    msg.innerHTML = '<div class="alert alert-warning">' + mensagem + '</div>';
-}
-
-function readJogo(processaDados) {
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            processaDados(data);
-        })
-        .catch(error => {
-            console.error('Erro ao ler jogos via API JSONServer:', error);
-            displayMessage("Erro ao ler jogos");
-        });
-}
-
-function createJogo(jogo, refreshFunction) {
-    fetch(apiUrl, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(jogo),
-    })
-        .then(response => response.json())
-        .then(data => {
-            displayMessage("Jogo inserido com sucesso");
-            if (refreshFunction)
-                refreshFunction();
-        })
-        .catch(error => {
-            console.error('Erro ao inserir jogo via API JSONServer:', error);
-            displayMessage("Erro ao inserir jogo");
-        });
-}
-
-function updateJogo(id, jogo, refreshFunction) {
-    fetch(`${apiUrl}/${id}`, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(jogo),
-    })
-        .then(response => response.json())
-        .then(data => {
-            displayMessage("Jogo alterado com sucesso");
-            if (refreshFunction)
-                refreshFunction();
-        })
-        .catch(error => {
-            console.error('Erro ao atualizar jogo via API JSONServer:', error);
-            displayMessage("Erro ao atualizar jogo");
-        });
-}
-
-function deleteJogo(id, refreshFunction) {
-    fetch(`${apiUrl}/${id}`, {
-        method: 'DELETE',
-    })
-        .then(response => response.json())
-        .then(data => {
-            displayMessage("Jogo removido com sucesso");
-            if (refreshFunction)
-                refreshFunction();
-        })
-        .catch(error => {
-            console.error('Erro ao remover jogo via API JSONServer:', error);
-            displayMessage("Erro ao remover jogo");
-        });
-}
+const inputQtd = document.getElementById("qtd-jogadores");
+inputQtd.addEventListener("input", () => {
+  const quantidades = inputQtd.value.split("-");
+  console.log(quantidades);
+  if (!validarQuantidade(quantidades)) {
+    inputQtd.setCustomValidity("Quantidade inválida. Ex: 1, 2-5.");
+  } else {
+    inputQtd.setCustomValidity("");
+  }
+});
