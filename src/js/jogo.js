7641e9654f512af487a82146b0a6765b5d5cae4a
Jahn16 <jpdeoliveiraferreira@gmail.com>
2025-06-17 19:59:43 -0300

    feat: add rating system

--- src/js/jogo.js
+++ src/js/jogo.js
@@ -6,14 +6,33 @@ document.getElementsByTagName("title");
 
 const jogos = JSON.parse(localStorage.getItem("jogos") || "[]");
 
-const jogo = jogos.find((j) => j.nome.toLowerCase() === nomeJogo.toLowerCase());
+const slug = (nome) => nome.toLowerCase().replaceAll(" ", "-");
+const idJogo = jogos.findIndex((j) => slug(j.nome) === slug(nomeJogo));
+const jogo = jogos[idJogo];
+
+const mostraAvaliacao = (nota) => {
+  const radio = document.getElementById(`star-${nota}`);
+  radio.checked = true;
+};
 
 if (jogo) {
   document.getElementById("imagem").src = jogo.imagem;
   document.getElementById("imagem").alt = jogo.nome;
   document.getElementById("titulo").textContent = jogo.nome;
   document.getElementById("descricao").textContent = jogo.descricao;
-  const avaliacaoGeral = document.getElementById("avaliacao-geral");
+  if (jogo.avaliacao) {
+    mostraAvaliacao(jogo.avaliacao);
+  }
 } else {
   alert("Jogo não encontrado.");
 }
+const avaliacaoInputs = document.querySelectorAll(
+  '#sua-avaliacao input[type="radio"]',
+);
+avaliacaoInputs.forEach((input) => {
+  input.addEventListener("change", (event) => {
+    jogo.avaliacao = parseInt(event.target.value);
+    jogos[idJogo] = jogo;
+    localStorage.setItem("jogos", JSON.stringify(jogos));
+  });
+});
